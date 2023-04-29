255be4e990e03b9408ba9b477dce99dfa3a11645
lostmart <lostmart@hotmail.com>
2023-04-29 16:33:49 +0200

    new accordion stylers

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,25 @@
 import style from './Accordion.module.scss'
+import txtData from '../data/aboutData.json'
+
 const Accordion = () => {
+	const handleClick = (e) => {
+		let panel = e.target.parentElement.nextElementSibling
+		panel.style.maxHeight
+			? (panel.style.maxHeight = null)
+			: (panel.style.maxHeight = panel.scrollHeight + 'px')
+	}
+
 	return (
-		<div className={style.accordion_item}>
-			<h2>
-				<button>Ahora</button>
-			</h2>
-			<div className={style.accordion_body}>
-				La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
-				comportement discriminatoire ou de perturbation du voisinage entraînera
-				une exclusion de notre plateforme.
-			</div>
-		</div>
+		<>
+			{txtData.map((panel, index) => (
+				<div key={index} className={style.accordion_item}>
+					<h2>
+						<button onClick={handleClick}>{panel.title}</button>
+					</h2>
+					<div className={style.accordion_body}>{panel.text}</div>
+				</div>
+			))}
+		</>
 	)
 }
 
