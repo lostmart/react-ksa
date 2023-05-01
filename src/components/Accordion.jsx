d5ee573872f361d6b7cadb4fb962e601f429a5e7
lostmart <lostmart@hotmail.com>
2023-05-01 21:27:08 +0200

    new styles dynamics and beautoiful

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,9 +4,16 @@ import txtData from '../data/aboutData.json'
 const Accordion = () => {
 	const handleClick = (e) => {
 		let panel = e.target.parentElement.nextElementSibling
-		panel.style.maxHeight
-			? (panel.style.maxHeight = null)
-			: (panel.style.maxHeight = panel.scrollHeight + 'px')
+		// panel.style.maxHeight
+		// 	? (panel.style.maxHeight = null)
+		// 	: (panel.style.maxHeight = panel.scrollHeight + 'px')
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null
+			panel.style.padding = '0 16px 0 16px'
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + 'px'
+			panel.style.padding = '18px 16px 12px 16px'
+		}
 	}
 
 	return (
