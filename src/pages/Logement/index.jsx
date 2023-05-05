36f4eccd4ad299178e1bc106645c00bfda01475b
lostmart <lostmart@hotmail.com>
2023-05-05 11:00:17 +0200

    new styles

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -3,46 +3,41 @@ import { useParams } from 'react-router-dom'
 import styles from './index.module.scss'
 import Tag from '../../components/Tag.jsx'
 import Star from '../../components/Star'
-import Greeter from '../../components/Greeter'
 import Accordion from '../../components/Accordion'
 import Carrousel from '../../components/Carrousel'
+import { GreeterRatings } from '../../components/base/GreeterRatings'
 
 const Logement = () => {
 	let { locationId } = useParams()
-	// find correct app
+	// find correct appartment
 	const logement = LogemontData.find(({ id }) => id === locationId)
 
 	const starList = []
 	for (let i = 0; i < 5; i++) {
 		const ratingIndx = Number(logement.rating - 1)
-		// check hgow many positive stars it has and then give it a red color
-		// i < ratingIndx
-		// 	? starList.push(<Star key={i} />)
-		// 	: starList.push(<Star fillColor="#cdcaca" key={i} />)
-
+		// check how many positive stars it has and then give it a red color
 		starList.push(
 			i < ratingIndx ? <Star key={i} /> : <Star fillColor="#cdcaca" key={i} />
 		)
 	}
 
-	//console.log(logement.rating)
 	return (
 		<section className={styles.logementContainer}>
 			<Carrousel imgsArray={logement.pictures} />
-			<h2>{logement.title}</h2>
-			<p> {logement.location} </p>
-			<div className={styles.tagsContainer}>
-				{logement.tags.map((tag, indx) => (
-					<Tag key={indx} title={tag} />
-				))}
-			</div>
-			<div className={styles.ratingAndHost}>
-				<div>{starList}</div>
-				<Greeter
+			<article>
+				<h2>{logement.title}</h2>
+				<p> {logement.location} </p>
+				<div className={styles.tagsContainer}>
+					{logement.tags.map((tag, indx) => (
+						<Tag key={indx} title={tag} />
+					))}
+				</div>
+				<GreeterRatings
+					starList={starList}
 					greeterName={logement.host.name}
 					avatarUrl={logement.host.picture}
 				/>
-			</div>
+			</article>
 			<Accordion title="Description" body={logement.description} />
 			<Accordion title="Équipements" body={logement.equipments} />
 		</section>
